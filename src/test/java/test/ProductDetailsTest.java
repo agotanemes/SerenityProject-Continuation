a54a938b8fbed98d8d3f69194def0cc7c8287222
agotanemes <agotanemes96@gmail.com>
2020-03-12 11:49:39 +0200

    ceva

--- src/test/java/test/ProductDetailsTest.java
+++ src/test/java/test/ProductDetailsTest.java
@@ -18,7 +18,7 @@ public class ProductDetailsTest extends BaseTest{
     @Steps
     ProductDetailsSteps productDetailsSteps;
     @Test
-    //aici dfjdhjugfkl
+    //aici
     public void getCartProduct(){
         String name="TORI TANK";
         String name1="Lafayette Convertible Dress";
